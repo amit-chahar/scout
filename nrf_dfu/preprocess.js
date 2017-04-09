f0dd60280a49145ea3cbe7d14c73aa1cd84e827c
Amit Chahar <amitchahar384@gmail.com>
2017-04-09 14:13:11 +0530

    disconnect method added

--- nrf_dfu/preprocess.js
+++ nrf_dfu/preprocess.js
@@ -1,141 +1,104 @@
 var noble = require('noble');
+var Promise = require('bluebird');
+var nobleP = Promise.promisifyAll(noble);
 
 var peripheralIdOrAddress = process.argv[2].toLowerCase();
 const peripheral_name = 'Device_1_fv1.1';
 
-String dfu_service_uuid = "8e400001-f315-4f60-9fb8-838830daea50";
-String dfu_char_uuid = "8e400001-f315-4f60-9fb8-838830daea50";
-String dfu_cccd_uuid = "000002902-0000-1000-8000-00805f9b34fb";
+const dfu_service_uuid = "8e400001-f315-4f60-9fb8-838830daea50";
+const dfu_char_uuid = "8e400001-f315-4f60-9fb8-838830daea50";
+const dfu_cccd_uuid = "000002902-0000-1000-8000-00805f9b34fb";
 
 
-noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
+noble.on('stateChange', function (state) {
+    if (state === 'poweredOn') {
+        start_scanning();
+    } else {
+        noble.stopScanning();
+    }
 });
 
-noble.on('discover', function(peripheral) {
-  if (peripheral.advertisement. localName === peripheral_name || peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
-    noble.stopScanning();
+var start_scanning = function () {
+    const allow_duplicates = false;
+    nobleP.startScanning([dfu_service_uuid], allow_duplicates)
+        .then(function () {
+            console.log("Scanning Started");
+        })
+        .catch(function (error) {
+            console.log("Error: starting scanning");
+        })
+};
 
-    console.log('peripheral with ID ' + peripheral.id + ' found');
-    var advertisement = peripheral.advertisement;
+noble.on('discover', function (peripheral) {
+    if (peripheral.advertisement.localName === peripheral_name || peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
+        noble.stopScanning();
 
-    var localName = advertisement.localName;
-    var txPowerLevel = advertisement.txPowerLevel;
-    var manufacturerData = advertisement.manufacturerData;
-    var serviceData = advertisement.serviceData;
-    var serviceUuids = advertisement.serviceUuids;
+        console.log('peripheral with ID ' + peripheral.id + ' found');
+        var advertisement = peripheral.advertisement;
 
-    if (localName) {
-      console.log('  Local Name        = ' + localName);
+        var localName = advertisement.localName;
+        var txPowerLevel = advertisement.txPowerLevel;
+        var manufacturerData = advertisement.manufacturerData;
+        var serviceData = advertisement.serviceData;
+        var serviceUuids = advertisement.serviceUuids;
+
+        if (localName) {
+            console.log('  Local Name        = ' + localName);
+        }
+
+        if (txPowerLevel) {
+            console.log('  TX Power Level    = ' + txPowerLevel);
+        }
+
+        if (manufacturerData) {
+            console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+        }
+
+        if (serviceData) {
+            console.log('  Service Data      = ' + serviceData);
+        }
+
+        if (serviceUuids) {
+            console.log('  Service UUIDs     = ' + serviceUuids);
+        }
+
+        console.log();
+
+        explore(peripheral);
     }
-
-    if (txPowerLevel) {
-      console.log('  TX Power Level    = ' + txPowerLevel);
-    }
-
-    if (manufacturerData) {
-      console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
-    }
-
-    if (serviceData) {
-      console.log('  Service Data      = ' + serviceData);
-    }
-
-    if (serviceUuids) {
-      console.log('  Service UUIDs     = ' + serviceUuids);
-    }
-
-    console.log();
-
-    explore(peripheral);
-  }
 });
 
 function explore(peripheral) {
-  console.log('services and characteristics:');
+    console.log('services and characteristics:');
 
-  peripheral.on('disconnect', function() {
-    console.log("Peripheral disconnected");
-    //process.exit(0);
-  });
-
-  peripheral.connect(function(error) {
-    peripheral.discoverServices([dfu_service_uuid], function(error, services) {
-	for(var i = 0; i < services.length; i++){
-	    var service = services[i];
-	    console.log("Found service with UUID: " + service.uuid);
-	    service.discoverCharacteristics([dfu_char_uuid], function(error, characteristics) {
-		for(var j = 0; j < characteristics.length; j++){
-		    var characteristic = characteristics[j];
-		    console.log("Found characteristic with UUID: " + characteristic.uuid);
-
-                async.series([
-                  function(callback) {
-                    characteristic.discoverDescriptors(function(error, descriptors) {
-                      async.detect(
-                        descriptors,
-                        function(descriptor, callback) {
-                          return callback(descriptor.uuid === '2901');
-                        },
-                        function(userDescriptionDescriptor){
-                          if (userDescriptionDescriptor) {
-                            userDescriptionDescriptor.readValue(function(error, data) {
-                              if (data) {
-                                characteristicInfo += ' (' + data.toString() + ')';
-                              }
-                              callback();
-                            });
-                          } else {
-                            callback();
-                          }
-                        }
-                      );
-                    });
-                  },
-                  function(callback) {
-                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
-
-                    if (characteristic.properties.indexOf('read') !== -1) {
-                      characteristic.read(function(error, data) {
-                        if (data) {
-                          var string = data.toString('ascii');
-
-                          characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
-                        }
-                        callback();
-                      });
-                    } else {
-                      callback();
-                    }
-                  },
-                  function() {
-                    console.log(characteristicInfo);
-                    characteristicIndex++;
-                    callback();
-                  }
-                ]);
-              },
-              function(error) {
-                serviceIndex++;
-                callback();
-              }
-            );
-          });
-        },
-        function (err) {
-          peripheral.disconnect();
-        }
-      );
+    peripheral.on('disconnect', function () {
+        console.log("Peripheral disconnected");
+        //process.exit(0);
     });
-  });
+
+    peripheral.connect()
+        .then(function () {
+            console.log("Connected to peripheral");
+            return peripheral.characteristics([dfu_char_uuid]);
+        })
+        .catch(function (error) {
+            console.log("Error: connecting peripheral");
+        })
+        .then(function (characteristics) {
+            if (characteristics.length == 1) {
+                console.log("DFU characteristic found");
+                characteristics[0].discoverDescriptors();
+            }
+        })
+        .then(function (descriptors) {
+            descriptors.forEach(function (descriptor) {
+                if (descriptor.uuid === '2901') {
+                    console.log("CCCD found");
+                }
+            })
+        })
+        .catch(function (e) {
+            console.log("Error: Disconnecting peripheral");
+        })
+
 }
-
-    Contact GitHub API Training Shop Blog About 
-
-    © 2017 GitHub, Inc. Terms Privacy Security Status Help 
-
-
